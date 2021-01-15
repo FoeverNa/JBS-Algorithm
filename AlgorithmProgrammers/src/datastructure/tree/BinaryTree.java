cf699397554093b38e4a0fd9f8cb013bed554498
FoeverNa <foevna@naver.com>
2021-01-15 15:34:31 +0900

    update BianaryTree

--- AlgorithmProgrammers/src/datastructure/tree/BinaryTree.java
+++ AlgorithmProgrammers/src/datastructure/tree/BinaryTree.java
@@ -1,55 +1,124 @@
-//package datastructure.tree;
-//
-//class BinaryTree {
-//    private int [] array;
-//
-//    public BinaryTree(int [] array) {
-//        this.array = array;
-//    }
-//
-//    public void preorder() {
-//        for (int i = 0; i < array.length; i++) {
-//            boolean isFound = false;
-//            int root = 0;
-//            while (!isFound) {
-//                if()
-//            }
-//
-//        }
-//
-//    }
-//
-//    public void inorder() {
-//
-//    }
-//
-//    public void postorder() {
-//
-//    }
-//
-//    public boolean bfs(int value) {
-//        return true;
-//    }
-//
-//    public boolean dfs(int value) {
-//        return true;
-//    }
-//}
-//
-//
-//class BinaryTreeTest {
-//    public static void main(String[] args) {
-//        int [] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        BinaryTree tree = new BinaryTree(array);
-//
-//        tree.preorder();
-//        tree.inorder();
-//        tree.postorder();
-//
-//        System.out.println(tree.bfs(2));
-//        System.out.println(tree.bfs(10));
-//
-//        System.out.println(tree.dfs(5));
-//        System.out.println(tree.dfs(14));
-//    }
-//}
+package datastructure.tree;
+
+class BinaryTree {
+    private final int [] array;
+    private boolean isFound;
+
+    public BinaryTree(int [] array) {
+        this.array = array;
+    }
+
+    public void preorder() {
+        preorderHelper(0);
+        System.out.println("");
+    }
+
+    private void preorderHelper(int index) {
+        System.out.print(array[index] + " ");
+
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        if(left < array.length) {
+            preorderHelper(left);
+        }
+        if (right < array.length) {
+            preorderHelper(right);
+        }
+    }
+
+    public void inorder() {
+        inorderHelper(0);
+        System.out.println(" ");
+    }
+    private void inorderHelper(int index) {
+
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+
+        if(left < array.length) {
+            inorderHelper(left);
+        }
+
+        System.out.print(array[index] + " ");
+
+        if(right < array.length) {
+            inorderHelper(right);
+        }
+
+    }
+
+    public void postorder() {
+        postorderHelper(0);
+        System.out.println(" ");
+    }
+
+    private void postorderHelper(int index) {
+
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+
+        if(left < array.length) {
+            postorderHelper(left);
+        }
+
+        if(right < array.length) {
+            postorderHelper(right);
+        }
+
+        System.out.print(array[index] + " ");
+
+    }
+
+    public boolean bfs(int value) {
+        for(int i : array) {
+            if(i  == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int value) {
+        isFound = false;
+        dfsHelper(0, value);
+        return isFound;
+    }
+
+    private void dfsHelper(int index, int value) {
+        if(isFound) {
+            return;
+        }
+        if(array[index] == value) {
+            isFound = true;
+            return;
+        }
+
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        if(left < array.length) {
+            dfsHelper(left, value);
+        }
+        if (right < array.length) {
+            dfsHelper(right, value);
+        }
+
+    }
+}
+
+
+class BinaryTreeTest {
+    public static void main(String[] args) {
+        int [] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        NodeBinaryTree tree = new NodeBinaryTree(array);
+
+        tree.preorder();
+        tree.inorder();
+        tree.postorder();
+
+        System.out.println(tree.bfs(2));
+        System.out.println(tree.bfs(11));
+
+        System.out.println(tree.dfs(5));
+        System.out.println(tree.dfs(14));
+    }
+}
