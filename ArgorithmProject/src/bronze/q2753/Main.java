28fd1c83fa74572145d3a581be2a890aae8e1df5
FoeverNa <foevna@naver.com>
2021-03-09 19:40:12 +0900

    백준 알고리즘 1일차

---
+++ ArgorithmProject/src/bronze/q2753/Main.java
@@ -0,0 +1,18 @@
+package bronze.q2753;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int year;
+        year = sc.nextInt();
+        int lunarYear = 0;
+
+        if (year % 4 ==0 && year % 100 != 0 || year % 400 == 0) {
+            lunarYear = 1;
+        }
+        System.out.println(lunarYear);
+    }
+}
---
+++ ArgorithmProject/src/bronze/q5543/Main.java
@@ -0,0 +1,17 @@
+package bronze.q5543;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        System.out.println(Math.min(a, Math.min(b, c)) + Math.min(d, e) - 50);
+    }
+}
