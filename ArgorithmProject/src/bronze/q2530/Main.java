a23fc61ab9fce1b6f5d6b8064534476e77d7765d
FoeverNa <foevna@naver.com>
2021-03-10 16:02:28 +0900

    백준 알고리즘 문제 2530

---
+++ ArgorithmProject/src/bronze/q2530/Main.java
@@ -0,0 +1,20 @@
+package bronze.q2530;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int hours = sc.nextInt();
+        int minutes = sc.nextInt();
+        int seconds = sc.nextInt();
+        int time = sc.nextInt();
+
+        hours  = (hours + ((minutes +((seconds+time)/60))/60)) % 24;
+        minutes = (minutes + (((seconds+time)/60))) % 60;
+        seconds = (seconds+time) % 60;
+
+        System.out.println(hours + " " + minutes + " " + seconds);
+    }
+}
