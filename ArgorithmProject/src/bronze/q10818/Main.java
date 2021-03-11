9ae49f22ef420cd1d70718dd547fc511bfb06803
FoeverNa <foevna@naver.com>
2021-03-11 10:40:17 +0900

    백준 알고리즘 문제 10818

---
+++ ArgorithmProject/src/bronze/q10818/Main.java
@@ -0,0 +1,27 @@
+package bronze.q10818;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array= new int[n];
+
+        for (int i = 0; i < n ; i++) {
+            array[i] = sc.nextInt();
+        }
+        int min = array[1];
+        int max = array[1];
+
+        for(int integer : array) {
+            if (integer >= max) {
+                max = integer;
+            } else if (integer < min) {
+                min = integer;  
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
