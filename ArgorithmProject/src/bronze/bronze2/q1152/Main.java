a22dba803efdfa7bf8d74a13128921faf4bcc5cc
FoeverNa <foevna@naver.com>
2021-03-12 14:20:32 +0900

    백준 알고리즘 문제 1152

---
+++ ArgorithmProject/src/bronze/bronze2/q1152/Main.java
@@ -0,0 +1,13 @@
+package bronze.bronze2.q1152;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        System.out.println(st.countTokens());
+    }
+}
