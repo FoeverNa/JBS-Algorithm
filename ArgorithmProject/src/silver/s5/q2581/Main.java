2ecb40f64ca92ab4c87eb799f6ccf30f76cde6cb
FoeverNa <foevna@naver.com>
2021-03-26 22:04:54 +0900

    백준 문제 풀이 2581

---
+++ ArgorithmProject/src/silver/s5/q2581/Main.java
@@ -0,0 +1,44 @@
+package silver.s5.q2581;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(bf.readLine());
+        int n = Integer.parseInt(bf.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = m; i <= n; i++) {
+            if (i == 1) {
+                continue;
+            }
+            boolean prime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime) {
+                sum += i;
+                if (i < min) {
+                    min = i;
+                }
+            }
+        }
+
+        if(sum != 0) {
+            System.out.println(sum);
+            System.out.println(min);
+        } else{
+            System.out.println(-1);
+        }
+    }
+}
