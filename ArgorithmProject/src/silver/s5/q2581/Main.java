69f779cfc3d9cf25ba073c2ee46a16e9dfa5cb30
FoeverNa <foevna@naver.com>
2021-03-26 22:05:56 +0900

    백준 문제 풀이 2581 n의 제곱근까지만 탐색

--- ArgorithmProject/src/silver/s5/q2581/Main.java
+++ ArgorithmProject/src/silver/s5/q2581/Main.java
@@ -19,7 +19,7 @@ public class Main {
                 continue;
             }
             boolean prime = true;
-            for (int j = 2; j < i; j++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     prime = false;
                     break;
