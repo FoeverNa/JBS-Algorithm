c014843d40437d79beea8eea907e6cf87e33e01f
FoeverNa <foevna@naver.com>
2021-03-27 09:54:58 +0900

    백준 문제풀이 2581 에라토스테네스의체 적용

--- ArgorithmProject/src/silver/s5/q2581/Main.java
+++ ArgorithmProject/src/silver/s5/q2581/Main.java
@@ -6,39 +6,53 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    public static boolean[] prime;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int m = Integer.parseInt(bf.readLine());
         int n = Integer.parseInt(bf.readLine());
 
-        int sum = 0;
+        make_prime(n);
+
+        int sum =0;
         int min = Integer.MAX_VALUE;
 
-        for (int i = m; i <= n; i++) {
-            if (i == 1) {
-                continue;
-            }
-            boolean prime = true;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    prime = false;
-                    break;
-                }
-            }
-
-            if (prime) {
+        for (int i = m; i < prime.length ; i++) {
+            if (!prime[i]) {
                 sum += i;
-                if (i < min) {
+                if (min > i) {
                     min = i;
                 }
             }
         }
-
-        if(sum != 0) {
+        if (sum == 0) {
+            System.out.println(-1);
+        } else  {
             System.out.println(sum);
             System.out.println(min);
-        } else{
-            System.out.println(-1);
         }
+
+    }
+
+    private static void make_prime(int n) {
+
+        prime = new boolean[n + 1];
+
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(prime[i]) {
+                continue;
+            }
+
+            for(int j = i*i; j < prime.length; j = j+i) {
+                prime[j] = true;
+            }
+        }
+
+
+
+
     }
 }
