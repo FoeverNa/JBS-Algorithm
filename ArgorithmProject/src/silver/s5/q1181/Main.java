9331649ce67978ae2ad6a6c959e06cf0c98cb3f7
FoeverNa <foevna@naver.com>
2021-03-29 11:01:45 +0900

    백준 문제풀이 1181 compare 메소드 간편화

--- ArgorithmProject/src/silver/s5/q1181/Main.java
+++ ArgorithmProject/src/silver/s5/q1181/Main.java
@@ -38,12 +38,6 @@ class Main {
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
-        if (a.length() > b.length()) {
-            return 1;
-        } else if (a.length() == b.length()) {
-            return 0;
-        } else {
-            return -1;
-        }
+        return a.length() - b.length();
     }
 }
