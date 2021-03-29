2cf3aaf90923ebdab893b0f1ab3a4889837f7cfa
FoeverNa <foevna@naver.com>
2021-03-29 11:00:22 +0900

    백준 문제풀이 1181

---
+++ ArgorithmProject/src/silver/s5/q1181/Main.java
@@ -0,0 +1,49 @@
+package silver.s5.q1181;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.HashSet;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bf.readLine());
+        Set<String> set = new HashSet<>();
+
+        for(int i = 0 ; i < t ; i++) {
+            set.add(bf.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        Collections.sort(list);
+
+        list.sort(new ListComparator());
+
+        for(String s : list) {
+            bw.write(s + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+
+class ListComparator implements Comparator<String> {
+    @Override
+    public int compare(String a, String b) {
+        if (a.length() > b.length()) {
+            return 1;
+        } else if (a.length() == b.length()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
