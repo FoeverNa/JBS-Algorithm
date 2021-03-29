6f60219ffc6540f621e9e7d3e6feccb85ca13b4c
FoeverNa <foevna@naver.com>
2021-03-29 20:15:04 +0900

    백준 문제풀이 1018 for문 하나 줄이고 64-painting으로 간편화

--- ArgorithmProject/src/silver/s5/q1018/Main.java
+++ ArgorithmProject/src/silver/s5/q1018/Main.java
@@ -29,40 +29,30 @@ class Main {
         for (int i = 0; i <= n-8; i++) {
             for (int j = 0; j <= m-8; j++) {
                 // 시작 값이 W인지 B인지 확인
-                for(int c = 0; c < 2; c++) {
-                    char start;
-                    if (c == 0) {
-                        start = 'B';
-                    } else{
-                        start = 'W';
-                    }
-                    painting = 0;
-                    here:
-                    for (int k = 0; k < 8 ; k++) {
-                        for (int l = 0; l <8 ; l++) {
-                            char w = arrays[i+k][j+l];
-                            //현재의 값과 예상값이 다르면 페인트 칠할곳 ++
-                            if (w != start) {
-                                painting++;
-                            }
-                            //예상값을 스위치한다
-                            start = changeWord(start);
-                            // 최소값을 초과하면 더이상 연산하지않도록 break
-                            if (painting >= min) {
-                                break here;
-                            }
+                char start = arrays[i][j];
+                painting = 0;
+                for (int k = 0; k < 8 ; k++) {
+                    for (int l = 0; l <8 ; l++) {
+                        char w = arrays[i+k][j+l];
+                        //현재의 값과 예상값이 다르면 페인트 칠할곳 ++
+                        if (w != start) {
+                            painting++;
                         }
-                        //줄바꿈과 동시에 한번더 글자바꿔서 맞춰주기
+                        //예상값을 스위치한다
                         start = changeWord(start);
+                        // 최소값을 초과하면 더이상 연산하지않도록 break
                     }
+                    //줄바꿈과 동시에 한번더 글자바꿔서 맞춰주기
+                    start = changeWord(start);
+                }
+                //시작값이 반대인 상황과 비교해 최소값 구하기
+                painting = Math.min(painting, 64 - painting);
 
-                    //8x8를 다돌고 나서 최소값이 맞드면 갱신
-                    if (min > painting) {
-                        min = painting;
-                    }
+                // 최소값보다 현재 구한 페인틜이 작다면 최소값에 대입
+                if (min > painting) {
+                    min = painting;
                 }
             }
-
         }
         // 최소값 출력
         System.out.println(min);
@@ -73,4 +63,6 @@ class Main {
         return c == 'W' ? 'B' : 'W';
     }
 }
+
+
 
