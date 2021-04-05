2312a0261fbc316ed45dd8ea5bc4e256cff7a8aa
FoeverNa <foevna@naver.com>
2021-04-05 15:41:32 +0900

    백준 문제풀이 1476 풀이 간편화

--- ArgorithmProject/src/silver/s5/q1476/Main.java
+++ ArgorithmProject/src/silver/s5/q1476/Main.java
@@ -16,32 +16,17 @@ class Main {
         int S = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 숫자를 증가시킬 int 변수들을 선언한다
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        
-        
-        // 각숫자에 1을 증가시키고 E S M 과 각각 비교해서 같은지 확인한다
-            // 각 숫자에 범위를 넘어가면 1을 대입시킨다
-        int count = 0;
+        // 연도를 계산할 변수 선언
+        int year = 0;
+
+
         while(true) {
-            a++;b++;c++;
-            if (a > 15) {
-                a = 1;
-            }
-            if(b > 28) {
-                b = 1;
-            }
-            if(c > 19) {
-                c = 1;
-            }
-            count++;
-            if (a == E && b == S && c == M) {
+            year++;
+            if ((year-E) % 15 == 0 && (year-S) % 28 == 0  && (year-M) % 19 ==0 ) {
                 break;
             }
         }
         // 더 횟수를 출력한다
-        System.out.println(count);
+        System.out.println(year);
     }
 }
