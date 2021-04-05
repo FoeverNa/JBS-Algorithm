78c90391f4aa95ef534920551906fdf92a39915e
FoeverNa <foevna@naver.com>
2021-04-05 16:50:30 +0900

    백준 문제풀이 1037 주석추가

--- ArgorithmProject/src/silver/s5/q1037/Main.java
+++ ArgorithmProject/src/silver/s5/q1037/Main.java
@@ -15,12 +15,14 @@ class Main {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        //입력값중에 가장 큰값과 가장 작은 값을 구한다 -> 약수는 쌍을 이루기 때문
         for(int i = 0; i < t; i++) {
             int val = Integer.parseInt(st.nextToken());
             min = Math.min(val, min);
             max = Math.max(val, max);
         }
 
+        //2약수의 값을 곱한 값이 N이 됨으로 곱해서 출력해준다
         long N = (long) min * max;
         System.out.println(N);
     }
