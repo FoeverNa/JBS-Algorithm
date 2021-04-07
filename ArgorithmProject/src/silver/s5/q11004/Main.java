e620ae1df95d44c0a4821b2eebc304c22e530c93
FoeverNa <foevna@naver.com>
2021-04-07 21:15:14 +0900

    백준 1104번 벙합정렬 -> QuickSelection 알고리즘으로 변경해서 풀이

--- ArgorithmProject/src/silver/s5/q11004/Main.java
+++ ArgorithmProject/src/silver/s5/q11004/Main.java
@@ -7,55 +7,67 @@ import java.util.StringTokenizer;
 
 class Main {
 
+    static int n, k;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] arr = new int[n];
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        mergeSort(arr);
-        System.out.println(arr[K-1]);
+
+        quickSelect(arr);
+        System.out.println(arr[k-1]);
     }
 
-    private static void mergeSort(int[] arr) {
-        int[] temp = new int[arr.length];
-        mergeSort(arr, temp, 0, arr.length - 1);
+    private static void quickSelect(int[] arr) {
+        quickSelect(arr, 0, arr.length-1);
     }
 
-    private static void mergeSort(int[] arr, int[] temp, int start, int end) {
-        if (start < end) {
-            int mid = (start + end) / 2;
-            mergeSort(arr, temp, start, mid);
-            mergeSort(arr, temp, mid + 1, end);
-            merge(arr, temp, start, mid, end);
+    private static void quickSelect(int[] arr, int start, int end) {
+
+        if ( start >= end) {
+            return;
         }
+
+        int pt = partition(arr, start, end);
+        if(pt+1 == k) {
+            return;
+        } else if (pt+1 > k) {
+            quickSelect(arr, start, pt-1);
+        } else if (pt+1 < k) {
+            quickSelect(arr, pt+1, end);
+        }
+
     }
 
-    private static void merge(int[] arr, int[] temp, int start, int mid, int end) {
-        for (int i = start; i <= end; i++) {
-            temp[i] = arr[i];
-        }
+    private static int partition(int[] arr, int start, int end) {
 
-        int part1 = start;
-        int part2 = mid + 1;
-        int index = start;
-        while (part1 <= mid && part2 <= end) {
-            if (temp[part1] <= temp[part2]) {
-                arr[index] = temp[part1];
-                part1++;
-            } else {
-                arr[index] = temp[part2];
-                part2++;
-            }
-            index++;
+        int mid = (start + end) /2;
+        swap(arr, start, mid);
+
+        int pivot = arr[start];
+
+        int i = start;
+        int j = end;
+        while(i < j) {
+            while (pivot < arr[j]) j--;
+            while (i < j && pivot >= arr[i]) i++;
+            swap(arr, i, j);
         }
-        for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i] = temp[part1 + i];
-        }
+        arr[start] = arr[i];
+        arr[i] = pivot;
+        return i;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a]  = arr[b];
+        arr[b] = temp;
     }
 }
