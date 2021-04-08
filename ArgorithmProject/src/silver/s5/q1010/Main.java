ee2514e2b1bf84258a98291f17c1e5ede83f5333
FoeverNa <foevna@naver.com>
2021-04-08 10:36:26 +0900

    백준 1010번 dp이용해서 풀이

---
+++ ArgorithmProject/src/silver/s5/q1010/Main.java
@@ -0,0 +1,35 @@
+package silver.s5.q1010;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            dp = new int[n+1][k+1];
+            sb.append(combination(n, k)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int combination(int n, int k) {
+        if(dp[n][k] != 0) {
+            return dp[n][k];
+        }
+        if(n == k || k == 0) {
+            return dp[n][k] = 1;
+        }
+        return dp[n][k] = combination(n-1, k-1) + combination(n-1, k);
+    }
+}
