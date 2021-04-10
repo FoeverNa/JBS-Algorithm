469ce3c85ebbaa596e03ec94c170e2be1a6edff4
FoeverNa <foevna@naver.com>
2021-04-10 11:47:52 +0900

    백준 1978번 풀이

---
+++ ArgorithmProject/src/silver/s4/q1978/Main.java
@@ -0,0 +1,41 @@
+package silver.s4.q1978;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        boolean[] arr = new boolean[1001];
+        for (int i = 0; i < n ; i++) {
+            int val = Integer.parseInt(st.nextToken());
+            arr[val] = true;
+        }
+        int result = che(arr);
+        System.out.println(result);
+    }
+
+    private static int che(boolean[] arr) {
+        int count = 0;
+
+        for (int i = 2; i <=1000 ; i++) {
+            for (int j = 2; j <= 500 ; j++) {
+                    if(i*j >1000) {
+                        break;
+                    }
+                    arr[i*j] = false;
+                }
+        }
+        for (int i = 2; i <arr.length ; i++) {
+            if (arr[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
