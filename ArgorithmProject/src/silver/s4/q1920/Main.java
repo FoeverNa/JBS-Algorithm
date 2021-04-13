c101fa84455b46c70e4200da12111c0fc47da445
FoeverNa <foevna@naver.com>
2021-04-13 14:18:03 +0900

    백준 문제풀이 1920번 이진탐색 적용

--- ArgorithmProject/src/silver/s4/q1920/Main.java
+++ ArgorithmProject/src/silver/s4/q1920/Main.java
@@ -6,6 +6,12 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 class Main {
 
     public static void main(String[] args) throws IOException {
@@ -13,32 +19,38 @@ class Main {
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int[] arr = new int[n];
+
         for (int i = 0; i < n ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        int min = arr[0];
-        int max = arr[arr.length-1];
 
         int m = Integer.parseInt(bf.readLine());
         st = new StringTokenizer(bf.readLine());
         StringBuilder sb = new StringBuilder();
-        int result;
         for (int i = 0; i < m ; i++) {
             int val = Integer.parseInt(st.nextToken());
-            result =0;
-            if(val >= min || val <= max) {
-                for (int j = 0; j < arr.length ; j++) {
-                    if( val < arr[j]) {
-                        break;
-                    }else if(val == arr[j]) {
-                        result=1;
-                        break;
-                    }
-                }
-            }
-            sb.append(result).append("\n");
+            sb.append(bs(arr, val)).append("\n");
         }
         System.out.println(sb);
+    }
+    private static int bs(int[] arr, int val) {
+        int first = 0;
+        int last = arr.length-1;
+        int mid = 0;
+
+        while(first <= last) {
+            mid = (first + last) / 2;
+            if(val == arr[mid]) {
+                return 1;
+            }
+            if(val > arr[mid]) {
+                first = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+
+        return 0;
     }
 }
