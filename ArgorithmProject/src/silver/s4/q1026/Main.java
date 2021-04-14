209cb8538d1423f93233fe211ebfbf9f1361977d
FoeverNa <foevna@naver.com>
2021-04-14 15:59:44 +0900

    백준 1026번 문제풀이 -> 퀵정렬 구현

--- ArgorithmProject/src/silver/s4/q1026/Main.java
+++ ArgorithmProject/src/silver/s4/q1026/Main.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class Main {
 
     public static void main(String[] args) throws IOException {
@@ -21,12 +26,56 @@ class Main {
             b[i] = Integer.parseInt(stB.nextToken());
         }
 
-        Arrays.sort(a);
-        Arrays.sort(b);
+        quickSort(a);
+        quickSort(b);
+
         int s = 0;
         for (int i = 0; i < n; i++) {
-            s += a[i] * b[(n-1)-i];
+            s += a[i] * b[(n - 1) - i];
         }
         System.out.println(s);
+    }
+
+    static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    static void quickSort(int[] arr, int start, int end) {
+
+        int part = partition(arr, start, end);
+
+        if(start < part - 1) {
+            quickSort(arr, start, part-1);
+        }
+        if(part < end) {
+            quickSort(arr, part, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+
+        while(start <= end ) {
+
+            while (arr[start] < pivot  ) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+
+            if(start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
